7b6de42e8b34a379bb7dc651d568eadc6ea0adc5
saikumar246 <52447619+saikumar246@users.noreply.github.com>
2019-07-02 15:35:48 +0530

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,10 @@
+vowel=("a","e","i","o","u")
+con=("b","b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z")
+n=str(input())
+if n in vowel:
+    print("Vowel")
+elif n in con:
+          print("Consonant")
+else:
+      print("invalid")
+      
